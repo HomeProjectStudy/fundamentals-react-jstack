7b3f11f41616b1c74e25586bf61e9083933173a2
G.J <gjconsultoriati@gmail.com>
2023-06-23 21:21:03 -0300

    sintax short with props

--- src/Components/Post/styled.js
+++ src/Components/Post/styled.js
@@ -1,10 +1,14 @@
-import styled  from "styled-components";
+import styled, { css }  from "styled-components";
 
 export const Container = styled.article`
   margin-bottom: 1.5rem;
 
-  opacity: ${(props) => props.removed ? 0.5 : 1};
-  color: ${(props) => props.removed ? '#F00' : '#fff'};
+  /* opacity: ${(props) => props.removed ? 0.5 : 1};
+  color: ${(props) => props.removed ? '#F00' : '#fff'}; */
+  ${(props) => css`
+  opacity: ${props.removed ? '#F00' : '#fff'};
+  color: ${props.removed ? '#F00' : '#fff'}
+  `}
 `
 
 export const Subtitle = styled.small`
